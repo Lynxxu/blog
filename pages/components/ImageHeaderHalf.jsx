892c5b68f0c32e8a675cfd2b0f6a71cb79091953
Lynx <75997156+Lynxxu@users.noreply.github.com>
2023-08-03 09:28:51 +0800

    bug fixes

--- pages/components/ImageHeaderHalf.jsx
+++ pages/components/ImageHeaderHalf.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default function ImageHeaderHalf({ text, path }) {
   return (
     <>
@@ -17,23 +16,3 @@ export default function ImageHeaderHalf({ text, path }) {
     </>
   );
 }
-=======
-export default function ImageHeaderHalf({ text, path }) {
-  return (
-    <>
-      <div
-        className="flex h-[50vh] items-center justify-center bg-cover bg-center"
-        style={{
-          backgroundImage: `url(${path})`,
-        }}
-      >
-        {text && (
-          <div className="font-serif text-[2rem] text-prana-white drop-shadow-xl md:text-[3rem] lg:text-[3.5rem]">
-            {text}
-          </div>
-        )}
-      </div>
-    </>
-  );
-}
->>>>>>> 338bb39a9a6b3b81e4078eaf1d1d66d02f90dd98
